2aefe50bf044d36ed012747d2044fbcfb4961fe0
master <master@SMLAB306PC08_25>
2025-05-06 21:42:41 -0500

    enviasods los cambios

--- rtipantuñaExamen/Views/Login.xaml.cs
+++ rtipantuñaExamen/Views/Login.xaml.cs
@@ -2,8 +2,39 @@ namespace rtipantuñaExamen.Views;
 
 public partial class Login : ContentPage
 {
-	public Login()
+    private string[] user = { "estudiante2025", "uisrael", "sistemas" };
+    private string[] password = { "moviles", "2025", "2025_1" };
+    public Login()
 	{
 		InitializeComponent();
 	}
+    public Login(string usuario, string contraseña)
+    {
+        InitializeComponent();
+    }
+    private async void btnIniciar_Clicked(object sender, EventArgs e)
+    {
+        string usuario = txtUsuario.Text;
+        string contraseña = txtContraseña.Text;
+        int index = Array.IndexOf(user, usuario);
+        if (index >= 0 && password[index] == contraseña)
+        {
+            _ = DisplayAlert("Bienvenido", $"Hola,{usuario}!", "Cerrar");
+            //Navigation.PushModalAsync(new Views.Home());
+            await Navigation.PushAsync(new Views.Registro(usuario));
+
+
+
+        }
+        else
+        {
+            _ = DisplayAlert("Error", "Usuario o Contraseña incorrectos", "cerrar");
+        }
+
+    }
+    private async void btnRegistrase_Clicked(object sender, EventArgs e)
+    {
+        //Navigation.PushModalAsync(new Views.Registro());
+        await Navigation.PushAsync(new Views.Registro());
+    }
 }
--- rtipantuñaExamen/Views/Registro.xaml.cs
+++ rtipantuñaExamen/Views/Registro.xaml.cs
@@ -5,5 +5,22 @@ public partial class Registro : ContentPage
 	public Registro()
 	{
 		InitializeComponent();
+        PikerVa.SelectedIndex = 0;
+        PikerCiudad.SelectedIndex = 0;
 	}
+    public Registro(string usuario)
+    {
+        InitializeComponent();
+    }
+    private void PickerVa_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+        var selected= PikerVa.SelectedItem?.ToString();
+        
+        
+    }
+    private void PickerCiudad_SelectedIndexChanged(Object sender, EventArgs e)
+    {
+    }
+
 }
